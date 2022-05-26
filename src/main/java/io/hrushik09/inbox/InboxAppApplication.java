16531f6e981f40eab18b07e1b299f00132f71260
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-05-26 23:33:59 +0530

    showing folders for a user

---
+++ src/main/java/io/hrushik09/inbox/InboxAppApplication.java
@@ -0,0 +1,34 @@
+package io.hrushik09.inbox;
+
+import io.hrushik09.inbox.folders.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.nio.file.Path;
+
+@SpringBootApplication
+@RestController
+public class InboxAppApplication {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(InboxAppApplication.class, args);
+    }
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+
+    @PostConstruct
+    public void init() {
+    }
+}
