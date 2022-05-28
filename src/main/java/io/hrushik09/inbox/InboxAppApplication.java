5a42b0e5d3aa37b3593c506bb6793ed18470d36d
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-05-28 16:23:18 +0530

    added EmailListItem support

--- src/main/java/io/hrushik09/inbox/InboxAppApplication.java
+++ src/main/java/io/hrushik09/inbox/InboxAppApplication.java
@@ -1,5 +1,10 @@
 package io.hrushik09.inbox;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import io.hrushik09.inbox.emaillist.EmailListItem;
+import io.hrushik09.inbox.emaillist.EmailListItemKey;
+import io.hrushik09.inbox.emaillist.EmailListItemRepository;
+import io.hrushik09.inbox.folders.Folder;
 import io.hrushik09.inbox.folders.FolderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.nio.file.Path;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +23,9 @@ public class InboxAppApplication {
 
     @Autowired
     FolderRepository folderRepository;
+
+    @Autowired
+    EmailListItemRepository emailListItemRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(InboxAppApplication.class, args);
@@ -26,5 +35,27 @@ public class InboxAppApplication {
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+
+    @PostConstruct
+    public void init() {
+        folderRepository.save(new Folder("hrushik09", "Inbox", "blue"));
+        folderRepository.save(new Folder("hrushik09", "Sent", "green"));
+        folderRepository.save(new Folder("hrushik09", "Important", "yellow"));
+
+        for (int i = 0; i < 10; i++) {
+            EmailListItemKey key = new EmailListItemKey();
+            key.setId("hrushik09");
+            key.setLabel("Inbox");
+            key.setTimeUUID(Uuids.timeBased());
+
+            EmailListItem item = new EmailListItem();
+            item.setKey(key);
+            item.setTo(List.of("hrushik09"));
+            item.setSubject("Sending a message " + i);
+            item.setUnread(true);
+
+            emailListItemRepository.save(item);
+        }
     }
 }
