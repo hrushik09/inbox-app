d13bee2c55d4a29abdf3d9496b3baead6b278d51
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-05-29 12:07:21 +0530

    added support for counting unread emails

--- src/main/java/io/hrushik09/inbox/InboxAppApplication.java
+++ src/main/java/io/hrushik09/inbox/InboxAppApplication.java
@@ -8,6 +8,7 @@ import io.hrushik09.inbox.emaillist.EmailListItemKey;
 import io.hrushik09.inbox.emaillist.EmailListItemRepository;
 import io.hrushik09.inbox.folders.Folder;
 import io.hrushik09.inbox.folders.FolderRepository;
+import io.hrushik09.inbox.folders.UnreadEmailStatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -32,6 +33,9 @@ public class InboxAppApplication {
     @Autowired
     EmailRepository emailRepository;
 
+    @Autowired
+    UnreadEmailStatsRepository unreadEmailStatsRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(InboxAppApplication.class, args);
     }
@@ -47,6 +51,10 @@ public class InboxAppApplication {
         folderRepository.save(new Folder("hrushik09", "Inbox", "blue"));
         folderRepository.save(new Folder("hrushik09", "Sent", "green"));
         folderRepository.save(new Folder("hrushik09", "Important", "yellow"));
+
+        unreadEmailStatsRepository.incrementUnreadCounter("hrushik09", "Inbox");
+        unreadEmailStatsRepository.incrementUnreadCounter("hrushik09", "Inbox");
+        unreadEmailStatsRepository.incrementUnreadCounter("hrushik09", "Inbox");
 
         for (int i = 0; i < 10; i++) {
             EmailListItemKey key = new EmailListItemKey();
---
+++ src/main/java/io/hrushik09/inbox/folders/UnreadEmailStatsRepository.java
@@ -0,0 +1,18 @@
+package io.hrushik09.inbox.folders;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UnreadEmailStatsRepository extends CassandraRepository<UnreadEmailStats, String> {
+    List<UnreadEmailStats> findAllById(String id);
+
+    @Query("update unread_email_stats set unreadcount = unreadcount + 1 where user_id = ?0 and label = ?1")
+    public void incrementUnreadCounter(String userId, String label);
+
+    @Query("update unread_email_stats set unreadcount = unreadcount - 1 where user_id = ?0 and label = ?1")
+    public void decrementUnreadCounter(String userId, String label);
+}
